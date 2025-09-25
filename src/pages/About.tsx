3c9fe11c741592ab372c9ed566350d999b379e4e
generalplanet54-byte <general.planet.54@gmail.com>
2025-09-25 16:21:45 +0200

    Added Contact.tsx

---
+++ src/pages/About.tsx
@@ -0,0 +1,10 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>Learn more about Ubani Hosting</p>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <p>Welcome to Ubani Hosting</p>
+    </div>
+  );
+};
+
+export default Home;
